c14f0c897b480313dd13acb84cc5f68c4bb45f86
hyeon seung jae <uiandwe@gmail.com>
2018-09-16 13:43:44 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.use("*", (req, res, next)=>{
+    res.send("hello");
+});
+
+
+app.listen(3000, ()=>{
+    console.log("server start");
+})
